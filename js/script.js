f80955431628f537c74be3299e33abc7436d1060
MelmanPim <59539663+MelmanPim@users.noreply.github.com>
2021-06-10 14:51:28 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const age = 16;
+const isFemale = false;
+const driverStatus = 'bob';
+
+if (age >= 18) {
+    console.log('Je bent ouder dan 18, je mag naar binnen!');
+} else {
+    console.log('Helaas, je mag niet naar binnen!');
+}
+
+if (isFemale) {
+    console.log('Welkom bij de ladiesnight')
+} else {
+    console.log('Hmm, met die baard hoor je hier niet thuis')
+}
+
+if (driverStatus == 'bob') {
+    console.log('Fijne avond verder')
+} else {
+    console.log('Dat BVOtje lijkt me niet zo een sterk plan')
+}
